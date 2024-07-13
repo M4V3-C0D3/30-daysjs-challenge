3b2978900e160de5bf10e0a270476dfb75c29b80
M4V3-C0D3 <mustafax70pro@gmail.com>
2024-07-14 03:39:14 +0530

    day-1 variables and data types

---
+++ datatypes.js
@@ -0,0 +1,37 @@
+var num = 33;
+console.log(num);
+
+let str = "it is string store in LET"
+console.log(str);
+
+const bol = true
+console.log(bol);
+let age = 38;
+console.log(`Age is type of ${typeof age}`);
+let named = "Mustsfa";
+console.log(`Named is type of${typeof named}`);
+const coder = true;
+console.log("Coder is type of", typeof coder);
+
+let programmer = {
+    age: 38,
+    language: "javascript",
+    framework: "nextjs",
+    react: true,
+}
+
+console.log([`programmer is type of ${typeof programmer}`])
+
+let codeProfile = [11, "Js",]
+console.log(`programmer is type of ${typeof codeProfile}`);
+
+
+let reuse = "resue the LET value";
+console.log(reuse)
+reuse = "reuse is re-assinged";
+console.log(reuse);
+
+const cantReuse = true;
+console.log(cantReuse)
+cantReuse = false;
+console.log(cantReuse)
