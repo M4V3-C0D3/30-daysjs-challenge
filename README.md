85f51f8b5f0857c488f16d6d79f0925b3b7bb04a
M4V3-C0D3 <mustafax70pro@gmail.com>
2024-07-14 20:41:54 +0530

    operators

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 30-daysjs-challenge
 
 # july-13-2024 Variables and data types completed
+
+# july-14-2024 Operators, ternary operators, comparison operators 
---
+++ operators.js
@@ -0,0 +1,37 @@
+let sum = (a, b) => {
+    return console.log(a + b);
+}
+
+sum(1, 4);
+let subs = (a, b) => {
+    return console.log(a - b);
+}
+subs(1, 4);
+
+let divide = (a, b) => {
+    return console.log(a / b);
+}
+divide(1, 4);
+
+let multiply = (a, b) => {
+    return console.log(a * b);
+}
+
+multiply(8, 7)
+a = 5;
+b = 4;
+
+a += 3;
+console.log(a)
+a -= 1;
+console.log(a)
+
+let cond1 = a > b;
+let cond2 = a === b;
+
+console.log(cond1 && cond2)
+let age = 25;
+
+let result = age > 18 ? "adult" : "not adult";
+
+console.log(result)
